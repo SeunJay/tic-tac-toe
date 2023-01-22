2dd4db0a87d60644154446852a46a72792d1dd07
seunjay <seunjay92@gmail.com>
2023-01-22 20:41:43 +0100

    game component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Game from "./pages/Game";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="">
+      <Game />
     </div>
   );
 }
